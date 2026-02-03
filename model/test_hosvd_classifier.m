9944fa42289ca456803dc1ec1a1e113fb6e0e4b4
Noah <noah.steidle@posteo.de>
2026-02-03 15:05:47 +0100

    Generarlized testing of hosvd classifier by switching from one-sample testing to multi-sample testing

--- model/test_hosvd_classifier.m
+++ model/test_hosvd_classifier.m
@@ -1,47 +1,66 @@
 
-function [errors] = test_hosvd_classifier(test_tensor, bases)
+function [preds] = test_hosvd_classifier(test_tensors, bases)
 %TEST_HOSVD_CLASSIFIER Run inference.
-%   test_tensor: Unknown sample to classify
+%   test_tensor: Unknown samples to classify
 %   bases: Trained models based on HOSVD
 
     %% Input checks
-    validateattributes(test_tensor, {'double'}, {});
+    validateattributes(test_tensors, {'double'}, {});
     validateattributes(bases, {'double'}, {});
 
     % Get size of tensor
-    tensor_size = size(test_tensor);
+    tensor_size = size(test_tensors);
+    % Get number of test samples
+    no_test_samples = size(test_tensors,1);
     % Get number of modes
     m = numel(tensor_size);
     % Get number of classes from bases
-    c = size(bases,1);
+    no_classes = size(bases,1);
 
     %% Test phase
 
-    % Pre-allocate array for errors
-    errors= zeros([c,1]);
+    % Pre-allocate array for predictions
+    preds = zeros(no_test_samples,1);
+    
+    % Loop test samples
+    for ind_test_sample=1:no_test_samples
 
-    % Normalize tensor
-    test_tensor_normal = test_tensor / norm(test_tensor,'fro');
+        % Get test sample
+        test_sample = test_tensors(ind_test_sample,:,:);
+    
+        % Squeze test sample
+        test_sample = squeeze(test_sample);
+    
+        % Normalize test sample
+        test_sample = test_sample/frob(test_sample);
 
-    % For each class
-    for c_cnt=1:c
+        % Pre-allocate array for residuals
+        residuals = zeros([1,no_classes]);
 
-        % ... get basis
-        otherdims = repmat({':'},1,ndims(bases)-1);
-        basis = squeeze(bases(c_cnt,otherdims{:}));
+        % Loop classes
+        for ind_class=1:no_classes
 
-        % ... compute error to basis
-        err = 1;
-        otherdims = repmat({':'},1,ndims(basis)-1);
-        for j=1:size(basis,m+1)
-            err = err - (sum( test_tensor_normal.*basis(otherdims{:},j), 'all'))^2;
+            % Get basis
+            otherdims = repmat({':'},1,ndims(bases)-1);
+            basis = squeeze(bases(ind_class,otherdims{:}));
+
+            % Compute residual
+            res = 1;
+            for j=1:size(basis,m+1)
+                res = res - (sum( test_sample.*squeeze(basis(otherdims{:},j)), 'all'))^2;
+            end
+
+            % Save residual
+            residuals(ind_class) = res;
         end
 
-        % ... save error
-        errors(c_cnt) = err;
+        % Determine index of minimum residual
+        [~,I] = min(residuals);
+
+        % Save prediction according to minimum residual
+        preds(ind_test_sample,1) = I;
     end
 
-    % Return errors
     return
    
 end
